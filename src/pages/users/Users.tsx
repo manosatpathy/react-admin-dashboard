55faec9c7d95e396e34bf50c3172c628532d5370
manosatpathy <manosatpathy@gmail.com>
2025-01-18 11:48:00 +0530

    feat: add Users page with DataTable component

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,56 @@
+import { GridColDef } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
+import { userRows } from "../../data";
 import "./users.scss";
 
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 80 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="avatar logo" />;
+    },
+  },
+  { field: "lastName", type: "string", headerName: "Last Name", width: 150 },
+  { field: "firstName", type: "string", headerName: "First Name", width: 150 },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 250,
+    editable: true,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 150,
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
+
 const Users = () => {
-  return <div className="users">Users</div>;
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable slug="users" columns={columns} rows={userRows} />
+    </div>
+  );
 };
 
 export default Users;
